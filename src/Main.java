02c05730f5bc102ffc07938e624d27aa522d7320
Nikita Kvasov <grobovskoi@mail.ru>
2025-09-06 15:18:25 +0300

    Изменена основная логика проекта: реализован функционал запроса через консоль пути к файлу, который в дальнейшем будет использоваться для парсинга

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число и нажмите <Enter>: ");
-        int a = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число нажмите <Enter>: ");
-        int b = new Scanner(System.in).nextInt();
-        System.out.println("Сумма чисел: " + (a + b));
-        System.out.println("Разность чисел: " + (a - b));
-        System.out.println("Произведение чисел: " + (a * b));
-        System.out.println("Частное чисел: " + ((double)a / b));
+        int validFileCount = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Файл не существует.");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указанный путь ведёт к папке, а не к файлу.");
+                continue;
+            }
+            validFileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + validFileCount);
+        }
     }
 }
